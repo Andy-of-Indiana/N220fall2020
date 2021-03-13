f7582776929fca8bceef41ad66af04bee4b91f1a
Andy-of-Indiana <77703549+Andy-of-Indiana@users.noreply.github.com>
2021-03-12 23:40:35 -0500

    Midterm finish

--- Exercises/CollisionTwo/js/app.js
+++ Exercises/CollisionTwo/js/app.js
@@ -1,54 +1,95 @@
-var centerRectX = 125;
-var centerRectY = 100;
-var centerRectWidth = 50;
-var centerRectHeight = 100;
+var canvasLength = 600;
+var canvasWidth = 800;
+var counter = 0;
 
-var bouncingRectX = 10;
-var bouncingRectY = 10;
-var bouncingRectWidth = 100;
-var bouncingRectHeight = 50;
-var bouncingRectSpeedX = 1;
-var bouncingRectSpeedY = 2;
+var paddle = {
+  xPos: 100,
+  yPos: 250,
+  width: 50,
+  length: 150,
+
+  up: function() {
+    this.yPos -= 20;
+  },
+  down: function() {
+    this.yPos += 20;
+  },
+}
+var ball = {
+  xPos: 700,
+  yPos: 300,
+  rad: 25,
+  width: 50,
+  length: 50,
+  xSpeed: 5,
+  ySpeed: 4
+}
 
 function setup() {
-  createCanvas(300, 300);
-  noFill();
+  createCanvas(canvasWidth, canvasLength);
 }
-
 function draw() {
-  background(64);
+  BG();
+  rect(paddle.xPos ,paddle.yPos, paddle.width, paddle.length);
+  if(keyIsDown(UP_ARROW)){
+    paddle.up();
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    paddle.down();
+  }
+  paddleProperties();
 
-  //draw the center rectangle
-  stroke(255);
-  rect(centerRectX, centerRectY, centerRectWidth, centerRectHeight);
-
-  //if I keep moving in my current X direction, will I collide with the center rectangle?
-  if (bouncingRectX + bouncingRectWidth + bouncingRectSpeedX > centerRectX && 
-      bouncingRectX + bouncingRectSpeedX < centerRectX + centerRectWidth && 
-      bouncingRectY + bouncingRectHeight > centerRectY && 
-      bouncingRectY < centerRectY + centerRectHeight) {
-    bouncingRectSpeedX *= -1;
-  }
-  //bounce off left and right edges of screen
-  else if(bouncingRectX < 0 || bouncingRectX + bouncingRectWidth > width){
-    bouncingRectSpeedX *= -1;
-  }
-  
-  //if I keep moving in my current Y direction, will I collide with the center rectangle?
-  if (bouncingRectX + bouncingRectWidth > centerRectX && 
-      bouncingRectX < centerRectX + centerRectWidth && 
-      bouncingRectY + bouncingRectHeight + bouncingRectSpeedY > centerRectY && 
-      bouncingRectY + bouncingRectSpeedY < centerRectY + centerRectHeight) {
-    bouncingRectSpeedY *= -1;
-  }
-  //bounce off top and bottom edges of screen
-  else if(bouncingRectY < 0 || bouncingRectY + bouncingRectHeight > height){
-    bouncingRectSpeedY *= -1;
-  }
-  
-  bouncingRectX += bouncingRectSpeedX;
-  bouncingRectY += bouncingRectSpeedY;
-
-  //draw the bouncing rectangle
-  rect(bouncingRectX, bouncingRectY, bouncingRectWidth, bouncingRectHeight);
+  circle(ball.xPos, ball.yPos, ball.rad);
+  ballProperties()
+console.log(counter)
 }
+
+function ballProperties() {
+  ball.xPos -= ball.xSpeed;
+  ball.yPos -= ball.ySpeed;
+
+
+  if(ball.xPos < 0) {
+    ball.xPos = 700;
+    ball.xSpeed = 5;
+    ball.ySpeed = 4;
+    counter = 0;
+  }
+  if (ball.xPos + ball.width  > paddle.xPos && ball.xPos < paddle.xPos + paddle.width && ball.yPos + ball.length > paddle.yPos && ball.yPos < paddle.yPos + paddle.length) {
+    ball.xSpeed += 1;
+    ball.xSpeed *= -1;
+    counter++;
+  }
+  else if(ball.xPos > canvasWidth){
+    ball.xSpeed *= -1;
+  }
+  if (ball.xPos + ball.width > paddle.xPos && ball.xPos < paddle.xPos + paddle.width && ball.yPos + ball.length  > paddle.yPos && ball.yPos  < paddle.yPos + paddle.length) {
+  ball.ySpeed += 1;
+  ball.ySpeed *= -1;
+}
+  else if(ball.yPos < 0 || ball.yPos > canvasLength){
+    ball.ySpeed *= -1;
+  }
+
+}
+function paddleProperties() {
+  if(paddle.yPos < 0){
+    paddle.yPos = 0;
+  }
+  if (paddle.yPos > (canvasLength - paddle.length)){
+    paddle.yPos = (canvasLength - paddle.length);
+  }
+}
+
+function BG()
+{
+  background(0);
+  fill(0,209,0);
+  noStroke();
+}
+
+//2. A ball/circle that starts moving to the left of the screen.
+//3. This ball bounces off the TOP, BOTTOM, and LEFT of the screen.
+//4. This ball bounces off the paddle (use hitTestPoint)
+//5. If the ball goes beyond the right of the screen, place the ball back at the center of the screen and set its velocity to the left again.
+//7. (optional) Display a "score" number on the screen that ticks up by 1 every time the player catches the ball with the paddle. Resets to zero when the ball resets.
--- labs/midtermReview/js/app.js
+++ labs/midtermReview/js/app.js
@@ -1,5 +1,6 @@
 var canvasLength = 600;
 var canvasWidth = 800;
+var counter = 0;
 
 var paddle = {
   xPos: 100,
@@ -26,6 +27,7 @@ var ball = {
 
 function setup() {
   createCanvas(canvasWidth, canvasLength);
+
 }
 function draw() {
   BG();
@@ -39,29 +41,35 @@ function draw() {
   paddleProperties();
 
   circle(ball.xPos, ball.yPos, ball.rad);
-  ballProperties()
+  ballProperties();
+  console.log(counter);
 
 }
 
 function ballProperties() {
   ball.xPos -= ball.xSpeed;
   ball.yPos -= ball.ySpeed;
-  if(ball.xPos < 0)
-  {
+
+  if(ball.xPos < 0) {
     ball.xPos = 700;
+    ball.xSpeed = 5;
+    ball.ySpeed = 4;
+    counter = 0;
   }
-  if (ball.xPos + ball.width + ball.xSpeed > paddle.xPos && ball.yPos + ball.xSpeed < paddle.xPos + paddle.width && ball.yPos + ball.length > paddle.yPos && ball.yPos < paddle.yPos + paddle.length) {
+  if (ball.xPos + ball.width  > paddle.xPos && ball.xPos < paddle.xPos + paddle.width && ball.yPos + ball.length > paddle.yPos && ball.yPos < paddle.yPos + paddle.length) {
+    ball.xSpeed += 1;
+    ball.xSpeed *= -1;
+    ball.ySpeed += 1;
+    ball.ySpeed *= -1;
+    counter++;
+  }
+  else if(ball.xPos > canvasWidth){
     ball.xSpeed *= -1;
   }
-  else if (ball.xPos > canvasWidth) {
-      ball.xSpeed *= -1;
-    }
-    if (ball.xPos + ball.width > paddle.xPos && ball.xPos < paddle.xPos + paddle.width && ball.yPos + ball.length + ball.ySpeed > paddle.yPos && ball.yPos + ball.ySpeed < paddle.yPos + paddle.length) {
-      ball.ySpeed *= -1;
-    }
-  else if (ball.yPos < 0  || ball.yPos > canvasLength) {
+  else if(ball.yPos < 0 || ball.yPos > canvasLength){
     ball.ySpeed *= -1;
   }
+
 }
 function paddleProperties() {
   if(paddle.yPos < 0){
@@ -78,9 +86,3 @@ function BG()
   fill(0,209,0);
   noStroke();
 }
-
-//2. A ball/circle that starts moving to the left of the screen.
-//3. This ball bounces off the TOP, BOTTOM, and LEFT of the screen.
-//4. This ball bounces off the paddle (use hitTestPoint)
-//5. If the ball goes beyond the right of the screen, place the ball back at the center of the screen and set its velocity to the left again.
-//7. (optional) Display a "score" number on the screen that ticks up by 1 every time the player catches the ball with the paddle. Resets to zero when the ball resets.
